3e51745c69b7fa2828479fd1aa38f1b5c2ea7cde
lexa <sokolov@ozero.net>
2017-11-17 05:07:34 +0300

    Created json database file

--- app/src/main/java/net/ozero/jsontrainingparsing/MainActivity.java
+++ app/src/main/java/net/ozero/jsontrainingparsing/MainActivity.java
@@ -3,6 +3,11 @@ package net.ozero.jsontrainingparsing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     String JSON_STRING = "{\"employee\":{\"name\":\"Abhishek Saini\",\"salary\":65000}}";
@@ -11,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String loadJSONFrommAsset() {
+        String json = null;
+        try {
+            InputStream inputStream = getAssets().open();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return null;
     }
 }
