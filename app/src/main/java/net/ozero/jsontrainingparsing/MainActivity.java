f1626fa41e2131e019a9571b02e7a33337118342
lexa <sokolov@ozero.net>
2017-11-17 17:37:41 +0300

    Items created from new json file, including just array

--- app/src/main/java/net/ozero/jsontrainingparsing/MainActivity.java
+++ app/src/main/java/net/ozero/jsontrainingparsing/MainActivity.java
@@ -27,37 +27,38 @@ public class MainActivity extends AppCompatActivity {
         try {
             ListView listView = findViewById(R.id.list_view);
             List<String> items = new ArrayList<>();
-            JSONObject root = new JSONObject(loadJSONFrommAsset());
-            JSONArray jsonArray = root.getJSONArray("array");
-            setTitle(root.getString("title"));
+            JSONArray root = new JSONArray(loadJSONFrommAsset("arraylist.json"));
 
-
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject object = jsonArray.getJSONObject(i);
-                items.add(object.getString("company"));
+            JSONObject rootTitle = new JSONObject(loadJSONFrommAsset("list.json"));
+            setTitle(rootTitle.getString("title"));
+//            JSONArray jsonArray = root.getJSONArray("array");
+//
+//
+//
+            for (int i = 0; i < root.length(); i++) {
+                items.add(root.getJSONObject(i).getString("company"));
             }
-
+//
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
 
             if (listView != null) {
                 listView.setAdapter(adapter);
             }
-
-
-
-            JSONObject nested = root.getJSONObject("nested");
-            Log.d("TAG","flag value " + nested.getBoolean("flag"));
+//
+//
+//
+//            JSONObject nested = root.getJSONObject("nested");
+//            Log.d("TAG","flag value " + nested.getBoolean("flag"));
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    public String loadJSONFrommAsset() {
+    public String loadJSONFrommAsset(String fileName) {
         String json = null;
         try {
-            InputStream inputStream = getAssets().open("list.json");
+            InputStream inputStream = getAssets().open(fileName);
             int size = inputStream.available();
             byte[] buffer = new byte[size];
             inputStream.read(buffer);
